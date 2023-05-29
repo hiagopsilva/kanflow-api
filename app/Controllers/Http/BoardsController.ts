8d0ece8723fd9346cf147e293dc1e1076d4140b8
Hiago Prates <hiagohps360@gmail.com>
2023-05-29 09:58:39 -0300

    feat: list boards

--- app/Controllers/Http/BoardsController.ts
+++ app/Controllers/Http/BoardsController.ts
@@ -1,5 +1,6 @@
 import type { HttpLoggedContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Board from 'App/Models/Board'
+import ListBoardValidator from 'App/Validators/ListBoardValidator'
 import SaveBoardValidator from 'App/Validators/SaveBoardValidator'
 
 export default class BoardsController {
@@ -21,6 +22,25 @@ export default class BoardsController {
       const boardList = await Board.query().where('id_user', user.id).whereNull('deleted_at')
 
       return response.json(boardList)
+    } catch (error) {
+      console.log({ error })
+      return error
+    }
+  }
+
+  public async list({ user, request, response }: HttpLoggedContextContract) {
+    try {
+      const {
+        params: { id },
+      } = await request.validate(ListBoardValidator)
+
+      const board = await Board.query()
+        .where('id_user', user.id)
+        .where('id', id)
+        .whereNull('deleted_at')
+        .first()
+
+      return response.json(board)
     } catch (error) {
       console.log({ error })
       return error
