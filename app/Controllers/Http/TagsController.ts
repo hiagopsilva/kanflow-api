faaeb6004c0d2e8659c28059ab50a4ec3a5619b9
Hiago Prates <hiagohps360@gmail.com>
2023-05-29 10:20:29 -0300

    feat: list tags

--- app/Controllers/Http/TagsController.ts
+++ app/Controllers/Http/TagsController.ts
@@ -1,6 +1,7 @@
 import type { HttpLoggedContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Tag from 'App/Models/Tag'
 import CreateTagValidator from 'App/Validators/CreateTagValidator'
+import ListTagValidator from 'App/Validators/ListTagValidator'
 
 export default class TagsController {
   public async create({ request, response }: HttpLoggedContextContract) {
@@ -15,5 +16,15 @@ export default class TagsController {
     const tags = await Tag.all()
 
     return response.json(tags)
+  }
+
+  public async list({ request, response }: HttpLoggedContextContract) {
+    const {
+      params: { id },
+    } = await request.validate(ListTagValidator)
+
+    const tag = await Tag.findByOrFail('id', id)
+
+    return response.json(tag)
   }
 }
